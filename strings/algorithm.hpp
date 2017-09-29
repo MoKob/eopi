39c47dacd594e341c0f5a0161ca78c5d1802a5cc
Moritz Kobitzsch <moritz@mapbox.com>
2017-09-29 12:32:24 +0200

    add rabin karp algorithm for string search

--- strings/algorithm.hpp
+++ strings/algorithm.hpp
@@ -2,11 +2,11 @@
 #define EOPI_STRINGS_HPP_
 
 #include <algorithm>
-#include <cmath>
-#include <string>
-#include <sstream>
-#include <vector>
 #include <bitset>
+#include <cmath>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace eopi {
 namespace strings {
@@ -52,16 +52,16 @@ inline std::string reverse_words(std::string sentence) {
 }
 
 inline void print_mnemonics_helper(std::uint32_t length, std::uint32_t digits,
-                            std::string mnemonic) {
+                                   std::string mnemonic) {
     const static constexpr char chars[][10] = {{},
-                                         {'a', 'b', 'c'},
-                                         {'d', 'e', 'f'},
-                                         {'g', 'h', 'i'},
-                                         {'j', 'k', 'l'},
-                                         {'m', 'n', 'o'},
-                                         {'p', 'q', 'r', 's'},
-                                         {'t', 'u', 'v'},
-                                         {'w', 'x', 'y', 'z'}};
+                                               {'a', 'b', 'c'},
+                                               {'d', 'e', 'f'},
+                                               {'g', 'h', 'i'},
+                                               {'j', 'k', 'l'},
+                                               {'m', 'n', 'o'},
+                                               {'p', 'q', 'r', 's'},
+                                               {'t', 'u', 'v'},
+                                               {'w', 'x', 'y', 'z'}};
 
     if (length == 0) {
         std::cout << mnemonic << std::endl;
@@ -85,125 +85,165 @@ inline void print_mnemonics_helper(std::uint32_t length, std::uint32_t digits,
 inline void print_mnemonics(std::uint32_t digits) {
     if (digits == 0) return;
 
-    auto const length = pow(10,floor(log10(digits)));
+    auto const length = pow(10, floor(log10(digits)));
     print_mnemonics_helper(length, digits, "");
 }
 
-// runlength encoding/decoding for non-digit strings (no error checking, requires valid input)
-namespace runlength
-{
-    inline std::string encode(std::string const& str)
-    {
+// runlength encoding/decoding for non-digit strings (no error checking,
+// requires valid input)
+namespace runlength {
+inline std::string encode(std::string const& str) {
+    std::ostringstream oss;
+    auto itr = str.begin();
+    while (itr != str.end()) {
+        auto end = std::find_if_not(itr, str.end(),
+                                    [itr](char const c) { return c == *itr; });
+        oss << std::distance(itr, end) << *itr;
+        itr = end;
+    }
+    return oss.str();
+}
+
+inline std::string decode(std::string const& str) {
+    std::istringstream iss(str);
+    std::ostringstream oss;
+    std::uint32_t count;
+    while (iss >> count) {
+        char c;
+        iss >> c;
+        while (count--) oss << c;
+    }
+    return oss.str();
+}
+}  // namespace runlength
+
+namespace elias_gamma_code {
+inline std::string encode(std::vector<std::uint32_t> const& data) {
+    auto const to_binary = [](auto const val) {
         std::ostringstream oss;
-        auto itr = str.begin();
-        while( itr != str.end() )
-        {
-            auto end = std::find_if_not(itr,str.end(),[itr](char const c){ return c == *itr; });
-            oss << std::distance(itr,end) << *itr;
-            itr = end;
-        }
-        return oss.str();
+        oss << std::bitset<32>(val);
+        auto const str = oss.str();
+        return str.substr(str.find_first_of("1"));
+    };
+
+    std::ostringstream result;
+    for (auto val : data) {
+        auto const str = to_binary(val);
+        auto len = str.size();
+        while (--len) result << "0";
+        result << str;
+    }
+    return result.str();
+}
+
+inline std::vector<std::uint32_t> decode(std::string const& encoded) {
+    std::vector<std::uint32_t> result;
+
+    std::size_t pos = 0;
+    while (pos < encoded.size()) {
+        auto start = encoded.find_first_of("1", pos);
+        auto len = start - pos + 1;
+        auto num = encoded.substr(start, len);
+        result.push_back(std::stoi(num, nullptr, 2));
+        pos = start + len;
     }
 
-    inline std::string decode(std::string const& str)
-    {
-        std::istringstream iss(str);
-        std::ostringstream oss;
-        std::uint32_t count;
-        while( iss >> count )
-        {
-            char c;
-            iss >> c;
-            while(count--)
-                oss << c;
-        }
-        return oss.str();
-    }
-} // namespace runlength
-
-namespace elias_gamma_code
-{
-    inline std::string encode(std::vector<std::uint32_t> const &data)
-    {
-        auto const to_binary = [](auto const val)
-        {
-            std::ostringstream oss;
-            oss << std::bitset<32>(val);
-            auto const str = oss.str();
-            return str.substr(str.find_first_of("1"));
-        };
-
-        std::ostringstream result;
-        for( auto val : data )
-        {
-            auto const str = to_binary(val);
-            auto len = str.size();
-            while(--len)
-                result << "0";
-            result << str;
-        }
-        return result.str();
-    }
-
-    inline std::vector<std::uint32_t> decode(std::string const& encoded)
-    {
-        std::vector<std::uint32_t> result;
-
-        std::size_t pos = 0;
-        while( pos < encoded.size())
-        {
-            auto start = encoded.find_first_of("1",pos);
-            auto len = start - pos + 1;
-            auto num = encoded.substr(start,len);
-            result.push_back(std::stoi(num,nullptr,2));
-            pos = start + len;
-        }
-
-        return result;
-    }
-} // namespace elias_gamma_code
+    return result;
+}
+}  // namespace elias_gamma_code
 
 // print justified text
-inline void print_justified(std::vector<std::string> const& text, std::uint32_t const justification)
-{
-    for( std::size_t pos = 0, end = 0; pos < text.size(); pos = end)
-    {
+inline void print_justified(std::vector<std::string> const& text,
+                            std::uint32_t const justification) {
+    for (std::size_t pos = 0, end = 0; pos < text.size(); pos = end) {
         std::uint32_t count = text[pos].size();
         ++end;
-        while( end < text.size() && count + text[end].size() + 1 <= justification )
-        {
+        while (end < text.size() &&
+               count + text[end].size() + 1 <= justification) {
             count += text[end].size() + 1;
             ++end;
         }
 
         // last line, print unjustified
-        if( end == text.size() )
-        {
+        if (end == text.size()) {
             std::cout << text[pos];
-            for( auto i = pos+1; i < end; ++i )
-            {
+            for (auto i = pos + 1; i < end; ++i) {
                 std::cout << " " << text[i];
             }
             std::cout << std::endl;
-        } else // print justified 
+        } else  // print justified
         {
             auto const n = end - pos;
             auto const spaces = justification - count + n - 1;
-            auto const sep = spaces / (n-1);
+            auto const sep = spaces / (n - 1);
 
             std::cout << text[pos];
-            for( auto i = 1; i < n; ++i)
-            {
+            for (auto i = 1; i < n; ++i) {
                 // add spaces + additional one if we got a remainder
-                for( auto k = 0; k < sep + ((i-1 < (spaces % (n-1))) ? 1 : 0); ++k )
+                for (auto k = 0;
+                     k < sep + ((i - 1 < (spaces % (n - 1))) ? 1 : 0); ++k)
                     std::cout << " ";
 
-                std::cout << text[pos+i];
+                std::cout << text[pos + i];
             }
             std::cout << std::endl;
         }
     }
 }
+
+// rabin karp string search, expecting only basic chars, can offer worse
+// complexity for mixed patterns
+inline std::size_t rabin_karp(const std::string& text,
+                              const std::string& pattern) {
+    // on large patterns, we cannot find the pattern in the text
+    if (pattern.size() > text.size()) return std::string::npos;
+
+    const constexpr std::uint32_t modul = 30402457;
+    const constexpr std::uint32_t base =
+        26;  // number of chars, ignoring capitalisation
+
+    std::uint32_t max_power = 1;
+    for (std::size_t i = 1; i < pattern.size(); ++i)
+        max_power = (max_power * base) % modul;
+
+    // interpret a string as number (base 26). We compute the value of the
+    // string modul a prime
+    auto const get_hash = [base, modul](auto begin, auto const end) {
+        std::int64_t hash = 0;
+        for (auto itr = begin; itr != end; ++itr)
+            hash = (hash * base + *itr) % modul;
+        return hash;
+    };
+
+    // compute current hashes
+    auto const pattern_hash = get_hash(pattern.begin(), pattern.end());
+    auto local_text_hash =
+        get_hash(text.begin(), text.begin() + pattern.size());
+
+    // check for collisions
+    auto const match = [&](const auto pos) {
+        return 0 == text.compare(pos - pattern.size(), pattern.size(), pattern);
+    };
+
+    auto pos = text.begin() + pattern.size();
+    for (std::size_t pos = pattern.size(); pos < text.size(); ++pos) {
+        if (pattern_hash == local_text_hash && match(pos))
+            return pos - pattern.size();
+
+        // advance the search, by moving the hash function. Updating the hash to
+        // reflect the next position. This removes power * t[0] (the largest
+        // number) and adds the new char
+        local_text_hash -= ((text[pos - pattern.size()] * max_power) % modul);
+        if (local_text_hash < 0) local_text_hash += modul;
+        local_text_hash = (local_text_hash * base + text[pos]) % modul;
+    }
+
+    // if there is a match at the final position
+    if ((pattern_hash == local_text_hash) && match(text.size()))
+        return text.size() - pattern.size();
+
+    return std::string::npos;
+};
 
 }  // namespace strings
 }  // namespace eopi
--- strings/test.cpp
+++ strings/test.cpp
@@ -43,21 +43,56 @@ int main() {
     cout << "Snakestring of \"Hello World!\n";
     cout << eopi::strings::snakestring("Hello World!") << endl;
 
-    cout << "RLE of: " << "aaabbcCCCddddd" << endl;
+    cout << "RLE of: "
+         << "aaabbcCCCddddd" << endl;
     auto const encoding = eopi::strings::runlength::encode("aaabbcCCCddddd");
     cout << encoding << endl;
     cout << "Decoded: " << eopi::strings::runlength::decode(encoding) << endl;
 
-    std::vector<std::uint32_t> egd = {13,13};
+    vector<uint32_t> egd = {13, 13};
     auto const encoded = eopi::strings::elias_gamma_code::encode(egd);
-    std::cout << "Egc: " << encoded << std::endl;
+    cout << "Egc: " << encoded << endl;
     auto const decoded = eopi::strings::elias_gamma_code::decode(encoded);
-    std::cout << "Decoded:";
-    for( auto v : decoded )
-        std::cout << " " << v;
-    std::cout << std::endl;
+    cout << "Decoded:";
+    for (auto v : decoded) cout << " " << v;
+    cout << endl;
 
-    std::vector<std::string> justified = {"Hallo", "Welt", "was", "ist", "mit", "dir", "denn", "los"};
+    vector<string> justified = {"Hallo", "Welt", "was",  "ist",
+                                "mit",   "dir",  "denn", "los"};
     eopi::strings::print_justified(justified, 11);
+
+    string text =
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem "
+        "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae "
+        "ab illo inventore veritatis et quasi architecto beatae vitae dicta "
+        "sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit "
+        "aspernatur aut odit aut fugit, sed quia consequuntur magni dolores "
+        "eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, "
+        "qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, "
+        "sed quia non numquam eius modi tempora incidunt ut labore et dolore "
+        "magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis "
+        "nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut "
+        "aliquid ex ea commodi consequatur? Quis autem vel eum iure "
+        "reprehenderit qui in ea voluptate velit esse quam nihil molestiae "
+        "consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla "
+        "pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus "
+        "qui blanditiis praesentium voluptatum deleniti atque corrupti quos "
+        "dolores et quas molestias excepturi sint occaecati cupiditate non "
+        "provident, similique sunt in culpa qui officia deserunt mollitia "
+        "animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis "
+        "est et expedita distinctio. Nam libero tempore, cum soluta nobis est "
+        "eligendi optio cumque nihil impedit quo minus id quod maxime placeat "
+        "facere possimus, omnis voluptas assumenda est, omnis dolor "
+        "repellendus. Temporibus autem quibusdam et aut officiis debitis aut "
+        "rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint "
+        "et molestiae non recusandae. Itaque earum rerum hic tenetur a "
+        "sapiente delectus, ut aut reiciendis voluptatibus maiores alias "
+        "consequatur aut perferendis doloribus asperiores repellat.";
+
+    string pattern = "sapiente";
+
+    auto pos = eopi::strings::rabin_karp(text, pattern);
+    cout << "Found pattern at: " << pos << std::endl;
+
     return 0;
 }
