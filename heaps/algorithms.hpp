d35eca1840b68bcfefacca646e995f16686577ff
Moritz Kobitzsch <{ID?}-{username}@users.noreply.github.com>
2017-11-02 09:53:47 +0100

    compare to kth largest

--- heaps/algorithms.hpp
+++ heaps/algorithms.hpp
@@ -28,6 +28,58 @@ inline std::int32_t bonusses(std::vector<std::int32_t> const &lines_of_codes) {
   return std::accumulate(tickets.begin(), tickets.end(), 0);
 }
 
+// compute the running median of a sequence
+// requires O(N) space + O(logN) time for each insert
+// Median is O(1)
+class RunningMedian {
+public:
+  // push a new element
+  void emplace(int value) {
+    min_queue.push(value);
+    if (min_queue.size() > max_queue.size() + 1) {
+      max_queue.push(min_queue.top());
+      min_queue.pop();
+    }
+  }
+
+  // get the current median
+  int median() const {
+    if (min_queue.size() == max_queue.size())
+      return 0.5 * (min_queue.top() + max_queue.top());
+    else
+      return min_queue.top();
+  }
+
+private:
+  std::priority_queue<int, std::vector<int>, std::greater<int>> min_queue;
+  std::priority_queue<int> max_queue;
+};
+
+void compare_helper(std::vector<std::int32_t> const &heap,
+                    std::size_t const pos, std::size_t const k,
+                    std::int32_t value, std::size_t &equal,
+                    std::size_t &larger) {
+  // there will be no more larger/equal values in the heap, if heap[pos] < value
+  if (larger >= k || pos >= heap.size() || heap[pos] < value)
+    return;
+
+  if (heap[pos] == value)
+    ++equal;
+  else
+    ++larger;
+
+  compare_helper(heap, 2 * (pos + 1) - 1, k, value, equal, larger);
+  compare_helper(heap, 2 * (pos + 1), k, value, equal, larger);
+}
+
+// -1,0,1 find the relative size of value in comparison to heap[kth]
+std::int32_t compare(std::vector<std::int32_t> const &heap, const std::size_t k,
+                     const std::int32_t value) {
+  std::size_t equal = 0, larger = 0;
+  compare_helper(heap, 0, k, value, equal, larger);
+  return larger >= k ? -1 : (larger + equal >= k ? 0 : 1);
+}
+
 } // namespace algorithms
 } // namespace heaps
 } // namespace eopi
--- test/heaps.cpp
+++ test/heaps.cpp
@@ -1,5 +1,6 @@
 #include "heaps/algorithms.hpp"
 
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 #include <vector>
@@ -8,8 +9,34 @@ using namespace std;
 
 int main() {
   {
-    vector<std::int32_t> code{200, 300, 400, 100};
+    vector<int32_t> code{200, 300, 400, 100};
     cout << "Expecting: 7, Got: " << eopi::heaps::algorithms::bonusses(code)
          << endl;
+  }
+  {
+    eopi::heaps::algorithms::RunningMedian median;
+    for (int i = 0; i < 20; ++i) {
+      auto val = rand() % 20;
+      cout << "Adding " << val;
+      median.emplace(val);
+      cout << " - Median: " << median.median() << endl;
+    }
+  }
+  {
+    vector<int> heap(20);
+    iota(heap.begin(), heap.end(), 0);
+    random_shuffle(heap.begin(), heap.end());
+    make_heap(heap.begin(), heap.end());
+    cout << "Heap:";
+    for (auto h : heap)
+      cout << " " << h;
+    cout << endl;
+
+    cout << "Compare 5th largest element to 10: "
+         << eopi::heaps::algorithms::compare(heap, 5, 10) << endl;
+    cout << "Compare 11th largest element to 10: "
+         << eopi::heaps::algorithms::compare(heap, 11, 10) << endl;
+    cout << "Compare 10th largest element to 10: "
+         << eopi::heaps::algorithms::compare(heap, 10, 10) << endl;
   }
 }
