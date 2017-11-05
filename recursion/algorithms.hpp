09633c8905336d5aeda24bccba55abe70232b5bf
Moritz Kobitzsch <{ID?}-{username}@users.noreply.github.com>
2017-11-05 13:32:09 +0100

    extended simple regex matching

--- recursion/algorithms.hpp
+++ recursion/algorithms.hpp
@@ -9,6 +9,7 @@ namespace recursion {
 namespace algorithms {
 
 namespace details {
+
 void hanoi_helper(std::int32_t from, std::int32_t to, std::uint32_t discs) {
   if (discs == 0)
     return;
@@ -17,10 +18,72 @@ void hanoi_helper(std::int32_t from, std::int32_t to, std::uint32_t discs) {
             << std::endl;
   hanoi_helper(3 - from - to, to, discs - 1);
 }
+
+// check a known position for a match
+bool match_strict(std::string const &str, std::size_t pos_str,
+                  std::string const &regex, std::size_t pos_regex) {
+  // advance the reg-ex from the current position
+  for (; pos_str < str.size() && pos_regex < regex.size();
+       ++pos_str, ++pos_regex) {
+    // not at end of line, but need to be?
+    if (regex[pos_regex] == '$')
+      return false;
+
+    // can match, but do not need to match
+    if (pos_regex + 1 < regex.size() && regex[pos_regex + 1] == '*') {
+      // zero length matching of char
+      std::size_t new_pos_str = pos_str;
+      do {
+        // match at str position, starting with all chars after a match for *
+        if (match_strict(str, new_pos_str, regex, pos_regex + 2))
+          return true;
+        ++new_pos_str;
+      } while (new_pos_str < str.size() &&
+               (regex[pos_regex] == '.' ||
+                (str[new_pos_str - 1] == regex[pos_regex])));
+    } else {
+      // can we match the curren char?
+      if (str[pos_str] != regex[pos_regex] && regex[pos_regex] != '.')
+        return false;
+    }
+  }
+
+  // end of line match
+  if (pos_regex < regex.size()) {
+    if (regex[pos_regex] == '$')
+      return true;
+    if ((regex.size() - pos_regex) % 2 == 1 && regex.back() != '$')
+      return false;
+    for (; pos_regex + 2 < regex.size(); pos_regex += 2) {
+      if (regex[pos_regex] != '*')
+        return false;
+    }
+    return true;
+  }
+
+  // matched all?
+  return true;
+}
+
 } // namespace details
 
 // print a sequence of moves to solve the towers of hanoi for n towers
 inline void hanoi(std::uint32_t n) { details::hanoi_helper(0, 1, n); }
+
+inline bool esre_match(std::string str, std::string esre) {
+  // if we start with X* or need to match in front
+  if ((esre.size() > 2 && esre[1] == '*') || esre.front() == '$')
+    return details::match_strict(str, 0, esre, (esre.front() == '$' ? 1 : 0));
+
+  for (std::size_t pos = 0; pos < str.size(); ++pos) {
+    // check all possible starting positions
+    if ((str[pos] == esre[0] || esre[0] == '.') &&
+        details::match_strict(str, pos, esre, 0))
+      return true;
+  }
+
+  return false;
+}
 
 } // namespace algorithms
 } // namespace recursion
--- test/recursion.cpp
+++ test/recursion.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
 
 #include "recursion/algorithms.hpp"
 
 using namespace std;
 
-int main() { eopi::recursion::algorithms::hanoi(4); }
+int main() {
+  eopi::recursion::algorithms::hanoi(4);
+  {
+    // esre matching
+    {
+      string text = "xyaW9123";
+      string ex1 = "^aW9$";
+      string ex2 = "^aW9";
+      string ex3 = "aW9$";
+      string ex4 = "aW9";
+      cout << "Matching \"" << text << "\" with \"" << ex1 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex1) << endl;
+      cout << "Matching \"" << text << "\" with \"" << ex2 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex2) << endl;
+      cout << "Matching \"" << text << "\" with \"" << ex3 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex3) << endl;
+      cout << "Matching \"" << text << "\" with \"" << ex4 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex4) << endl;
+    }
+    {
+      string text = "aw9999b";
+      string ex1 = "aw9*b";
+      cout << "Matching \"" << text << "\" with \"" << ex1 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex1) << endl;
+    }
+    {
+      string text = "awb";
+      string ex1 = "aw9*b";
+      cout << "Matching \"" << text << "\" with \"" << ex1 << "\" yields: "
+           << eopi::recursion::algorithms::esre_match(text, ex1) << endl;
+    }
+  }
+}
